8ac35fe676332f4d6fa80a73277db95af4860fde
Alejandro  Mauricio <abecerraguz@MacBook-Pro-de-Alejandro.local>
2020-10-26 20:34:57 -0300

    Hook personalizado con louding

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
+
+import { useHttp } from './hooks/useHttp'
 
 const App = () => {
-  const [post, setPost] = useState([])
-    // Creamos otro estado que guarde el ID
-    const [id, setId] = useState(1);
 
-  useEffect(() => {
-      const getPost = async () => {
-      const res =  await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
-      const data = await res.json()
-      setPost(data)
-    }
+  const [id, setId] = useState(1)
+  const [post, isLoading] = useHttp(`https://jsonplaceholder.typicode.com/posts/${id}`)
 
-    getPost()
-  }, [id]);
+  const handlePrevId = () => {
+    if (id > 1) setId(id - 1)
+  }
 
-    const handlePrevId = () => {
-      if(id > 1) setId(id -1);
-    }
-
-    const handleNexId = () => {
-      setId(id + 1);
-    }
+  const handleNextId = () => {
+    setId(id + 1)
+  }
 
   return (
     <>
       <h1>{post.title}</h1>
-      <h6>{post.id}</h6>
       <p>{post.body}</p>
-      <button onClick={handlePrevId}>Prev ID</button>
-      <button onClick={handleNexId}>Next ID</button>
+      <button disabled={isLoading} onClick={handlePrevId}>Prev Id</button>
+      <button disabled={isLoading} onClick={handleNextId}>Next Id</button>
+      {isLoading && <h2>Loading...</h2>}
+      <p>{id}</p>
     </>
   );
 }
